79a407fb9469c08fa68b15209634f71a12761e43
aniobiobinna85@yahoo.com <aniobiobinna85@yahoo.com>
2023-05-17 12:38:42 +0200

    updated the express app

--- server.js
+++ server.js
@@ -6,6 +6,47 @@ app.get('/', (request, response) => {
   response.send('Hello World!')
 })
 
+async function insertStudent(data) {
+    let client
+    try{
+        client = new Client({
+             //you have this
+             host: 'dpg-ch3bqequt4m9tsckm0cg-a.frankfurt-postgres.render.com',
+                    port: 5432,
+                    database: 'learning_sql_1y7q',
+                    user: 'peter',
+                    password: 'NPq68LzqToDhloxMYVw6dm8Q1XQYyw9Y',
+                    ssl: { rejectUnauthorized: false}
+          })
+        await client.connect()
+        await client.query(`
+        insert into public.class_students(name,email_address)VALUES('${data.name}', '${data.email}' )`)
+    }catch(e){
+        console.error(`[ERROR in insertUser]: ${e}`);
+        throw e;
+    }finally{
+        client.end()
+    }
+  }
+app.post('/create-student', async (request, response)=>{
+  // write the code to insert some hard coded student in PG-SQL(on render)
+  let message = "", status = 0;
+  try{
+    const data = request.body()
+    await insertStudent(data)
+    message = `SUCCESS - I was able to create a student`
+    status = 201;
+  }catch(error){
+    console.error(`ERROR in POST`)
+    message = `ERROR- I could not create the student`
+    status = 500;
+  } finally {
+    response.status(status).send(message)
+  }
+
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
